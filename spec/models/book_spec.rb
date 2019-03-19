dc7d4a99cf6a93b93e7641e696203b88663e64e9
wipegup <wipegup@gmail.com>
2019-03-19 12:21:16 -0600

    Add Validations for book
    Co-authored-by: Corey Sheesley <43529041+CSheesley@users.noreply.github.com>

---
+++ spec/models/book_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Book, type: :model do
+  describe 'Validations' do
+    it { should validate_presence_of :title}
+    it { should validate_presence_of :pages}
+    it { should validate_presence_of :year}
+    it { should validate_presence_of :cover}
+  end
+
+end
