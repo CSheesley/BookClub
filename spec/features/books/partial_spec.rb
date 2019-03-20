bb4d89a4373fe2e8ac2a3eeb2491041c1b5c7849
wipegup <wipegup@gmail.com>
2019-03-20 17:14:34 -0600

    Split up render tests

--- spec/features/books/partial_spec.rb
+++ spec/features/books/partial_spec.rb
@@ -27,10 +27,9 @@ RSpec.describe 'book partial render', type: :view do
 
     book_2 = author_2.books.create(title: "Title_2", pages: 400, year: 1998, cover: "othermadeupurl.com")
     book_2.authors << author_3
-  end
     render book_2
 
-    binding.pry
+    # binding.pry
     expect(response).to have_xpath("//div[@id='book-card-#{book_1.id}']")
     within "#book-card-#{book_2.id}" do
       expect(response).to have_content(book_2.title)
