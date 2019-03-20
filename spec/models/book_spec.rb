835894938f9910895b40f972ace87f692c2f17bf
wipegup <wipegup@gmail.com>
2019-03-20 14:57:40 -0600

    Add test for new overridden authors method (instance method)
    Co-authored-by: Corey Sheesley <43529041+CSheesley@users.noreply.github.com>

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -16,7 +16,17 @@ RSpec.describe Book, type: :model do
 
   describe 'Class Method' do
     it "overrides the .authors method; returns a list of strings(names)" do
-      
+      author_1 = Author.create(name: "J.R.R Tolkein")
+      author_2 = Author.create(name: "William Peterson")
+      author_3 = Author.create(name: "Corey Sheesley")
+
+      book_1 = author_1.books.create(title: "The Hobbit", pages: 200, year: 1999, cover: "madeupurl.com")
+      book_2 = author_2.books.create(title: "Title_2", pages: 400, year: 1998, cover: "othermadeupurl.com")
+      book_2.authors << author_3
+      book_3 = author_2.books.create(title: "Art of Data", pages: 400, year: 2000, cover: "otherothermadeupurl.com")
+
+      expect(book_1.authors).to eq(["J.R.R Tolkein"])
+      expect(book_2.authors).to eq(["William Peterson", "Corey Sheesley"])
     end
   end
 end
