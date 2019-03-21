a684d706a0f7086b23f99a7cdefd3280101d4efe
Corey Sheesley <corey.sheesley@gmail.com>
2019-03-21 12:50:17 -0600

    Built functioning .authors_from_string method
    Co-authored-by: William Peterson <29714514+wipegup@users.noreply.github.com>

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,4 +4,10 @@ class Author < ApplicationRecord
   has_many :book_authors
   has_many :books, through: :book_authors
 
+  def self.authors_from_string(author_string)
+    string_list = author_string.split(",")
+    string_list.map do |string|
+      self.create(name: string.strip.titleize)
+    end
+  end
 end
