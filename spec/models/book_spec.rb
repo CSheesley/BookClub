f4ba53364b33a4e7cebedd21151e730740682c41
wipegup <wipegup@gmail.com>
2019-03-21 14:09:55 -0600

    Add test for sorting by number of reviews

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -45,7 +45,7 @@ RSpec.describe Book, type: :model do
   end
 
   describe 'Class Method' do
-    it "can sort books by number of pages" do
+    it ".sort_pages can sort books by number of pages, When given a symbol of :asc of :desc" do
       author_1 = Author.create(name: "J.R.R Tolkein")
       author_2 = Author.create(name: "William Peterson")
       author_3 = Author.create(name: "Corey Sheesley")
@@ -60,6 +60,31 @@ RSpec.describe Book, type: :model do
 
       expect(ascending_pages).to eq([book_1, book_3, book_2])
       expect(decending_pages).to eq([book_2, book_3, book_1])
+    end
+
+    it '.sort_num_reviews can sort books by number of reviews, when given a symbol of :asc or :desc' do
+      author_1 = Author.create(name: "J.R.R Tolkein")
+      author_2 = Author.create(name: "William Peterson")
+      author_3 = Author.create(name: "Corey Sheesley")
+
+      book_1 = author_1.books.create(title: "The Hobbit", pages: 200, year: 1999, cover: "madeupurl.com")
+      book_2 = author_2.books.create(title: "Title_2", pages: 400, year: 1998, cover: "othermadeupurl.com")
+      book_2.authors << author_3
+      book_3 = author_2.books.create(title: "Art of Data", pages: 400, year: 2000, cover: "otherothermadeupurl.com")
+
+      review_1 = book_1.reviews.create(title: "Great Book" , text: "What an adventure", rating: 5, user: "User_1")
+      review_2 = book_2.reviews.create(title: "Nice Read" , text: "Very enjoyable", rating: 4, user: "User_1")
+      review_3 = book_2.reviews.create(title: "If you have too" , text: "Meh", rating: 4, user: "User_2")
+      review_4 = book_3.reviews.create(title: "Data Pro" , text: "What a list!", rating: 2, user: "User_2")
+      review_5 = book_3.reviews.create(title: "Meh", text: "no doubt", rating: 1, user: "User_1")
+      review_6 = book_2.reviews.create(title:"Mediocre", text: "But surely passable", rating: 5, user: "User_3")
+
+      ascending_num_reviews = Book.sort_num_reviews(:asc)
+      descending_num_reviews = Book.sort_num_reviews(:desc)
+
+      expect(ascending_num_reviews).to eq([book_1, book_3, book_2])
+      expect(ascending_num_reviews).to eq([book_2, book_3, book_1])
+
     end
   end
 end
