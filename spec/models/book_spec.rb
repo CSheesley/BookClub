6e8c5856c33f638556dc291a5cf6acea9ce24ef0
wipegup <wipegup@gmail.com>
2019-03-21 13:31:44 -0600

    Add test for sorting by pages

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Book, type: :model do
     it { should have_many :reviews }
   end
 
-  describe 'Class Method' do
+  describe 'Instance Method' do
     it "new .author_names method; returns a list of strings(names)" do
       author_1 = Author.create(name: "J.R.R Tolkein")
       author_2 = Author.create(name: "William Peterson")
@@ -41,6 +41,25 @@ RSpec.describe Book, type: :model do
 
       expect(book_1.co_authors(author_1)).to eq([])
       expect(book_2.co_authors(author_2)).to eq(["Corey Sheesley"])
+    end
+  end
+
+  describe 'Class Method' do
+    it "can sort books by number of pages" do
+      author_1 = Author.create(name: "J.R.R Tolkein")
+      author_2 = Author.create(name: "William Peterson")
+      author_3 = Author.create(name: "Corey Sheesley")
+
+      book_1 = author_1.books.create(title: "The Hobbit", pages: 300, year: 1999, cover: "madeupurl.com")
+      book_2 = author_2.books.create(title: "Title_2", pages: 450, year: 1998, cover: "othermadeupurl.com")
+      book_2.authors << author_3
+      book_3 = author_2.books.create(title: "Art of Data", pages: 400, year: 2000, cover: "otherothermadeupurl.com")
+
+      ascending_pages = Book.sort_pages(:asc)
+      decending_pages = Book.sort_pages(:desc)
+
+      expect(ascending_pages).to eq([book_1, book_3, book_2])
+      expect(decending_pages).to eq([book_2, book_3, book_1])
     end
   end
 end
