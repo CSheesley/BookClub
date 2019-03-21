cb72ef1e0f79a6ee21ebd6d0188695d83857ac16
wipegup <wipegup@gmail.com>
2019-03-21 14:11:18 -0600

    Add test for sorting books by average reviews

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -84,7 +84,30 @@ RSpec.describe Book, type: :model do
 
       expect(ascending_num_reviews).to eq([book_1, book_3, book_2])
       expect(ascending_num_reviews).to eq([book_2, book_3, book_1])
+    end
 
+    it '.sort_avg_reviews can sort books by average rating of reviews, when given a symbol of :asc or :desc' do
+      author_1 = Author.create(name: "J.R.R Tolkein")
+      author_2 = Author.create(name: "William Peterson")
+      author_3 = Author.create(name: "Corey Sheesley")
+
+      book_1 = author_1.books.create(title: "The Hobbit", pages: 200, year: 1999, cover: "madeupurl.com")
+      book_2 = author_2.books.create(title: "Title_2", pages: 400, year: 1998, cover: "othermadeupurl.com")
+      book_2.authors << author_3
+      book_3 = author_2.books.create(title: "Art of Data", pages: 400, year: 2000, cover: "otherothermadeupurl.com")
+
+      review_1 = book_1.reviews.create(title: "Great Book" , text: "What an adventure", rating: 5, user: "User_1")
+      review_2 = book_2.reviews.create(title: "Nice Read" , text: "Very enjoyable", rating: 4, user: "User_1")
+      review_3 = book_2.reviews.create(title: "If you have too" , text: "Meh", rating: 4, user: "User_2")
+      review_4 = book_3.reviews.create(title: "Data Pro" , text: "What a list!", rating: 2, user: "User_2")
+      review_5 = book_3.reviews.create(title: "Meh", text: "no doubt", rating: 1, user: "User_1")
+      review_6 = book_2.reviews.create(title:"Mediocre", text: "But surely passable", rating: 5, user: "User_3")
+
+      ascending_avg_reviews = Book.sort_avg_reviews(:asc)
+      descending_avg_reviews = Book.sort_avg_reviews(:desc)
+
+      expect(ascending_avg_reviews).to eq([book_1, book_3, book_2])
+      expect(ascending_avg_reviews).to eq([book_2, book_3, book_1])
     end
   end
 end
