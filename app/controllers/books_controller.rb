a6954655bc1d926a8e48a45ab5ee5d04d8129b5a
Corey Sheesley <corey.sheesley@gmail.com>
2019-03-20 20:42:23 -0600

    Adds create and book_params actions to BooksController

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,4 +3,18 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @reviews = Review.where(book_id: params[:id])
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.create(book_params)
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :authors, :pages, :cover)
+  end
 end
