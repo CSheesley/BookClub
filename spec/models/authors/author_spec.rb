40eed009571e98132913364b4c64d25258da3ca4
Corey Sheesley <corey.sheesley@gmail.com>
2019-03-21 12:33:32 -0600

    Creates author spec test for .authors_from_string
    Co-authored-by: William Peterson <29714514+wipegup@users.noreply.github.com>

--- spec/models/authors/author_spec.rb
+++ spec/models/authors/author_spec.rb
@@ -8,7 +8,22 @@ RSpec.describe Author, type: :model do
   describe 'Relationships' do
     it { should have_many :book_authors}
     it { should have_many(:books).through :book_authors }
-    
   end
 
+  describe 'Class Methods' do
+    it '.authors_from_string creates list of authors' do
+      author_string_1 = "Dan Brown"
+      author_string_2 = "Dan Brown, corey sheesley"
+
+      author_list_1 = Author.authors_from_string(author_string_1)
+      author_list_2 = Author.authors_from_string(author_string_2)
+
+      expect(author_list_1[0].name).to eq(author_string_1)
+      expect(author_list_1.length).to eq(1)
+
+      expect(author_list_1[0].name).to eq("Dan Brown")
+      expect(author_list_1[1].name).to eq("Corey Sheesley")
+      expect(author_list_1.length).to eq(2)
+    end
+  end
 end
