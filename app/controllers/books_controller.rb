86095b554cc7737fe40818760af18b77fb4d459e
Corey Sheesley <corey.sheesley@gmail.com>
2019-03-21 12:15:42 -0600

    Adds logic to create action of book contoller
    Co-authored-by: William Peterson <29714514+wipegup@users.noreply.github.com>

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,4 +3,23 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @reviews = Review.where(book_id: params[:id])
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book_info = book_params
+    @author = Author.create(name: book_info.delete(:authors))
+    @book = @author.books.new(book_info)
+    @book.save
+    redirect_to book_path(@book)
+  end
+
+  private
+
+  def book_params
+    # binding.pry
+    params.require(:book).permit(:title, :authors, :pages, :year, :cover)
+  end
 end
