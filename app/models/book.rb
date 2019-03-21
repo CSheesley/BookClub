4a7de7941a579aaf963200a87c84b6e92d4e53d1
Corey Sheesley <corey.sheesley@gmail.com>
2019-03-21 15:47:41 -0600

    Now uses symbols in new_from_form in book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,7 @@ class Book < ApplicationRecord
   validates_presence_of :pages
   validates_presence_of :year
   validates_presence_of :cover
+  validates :pages, numericality: { greater_than_or_equal_to: 1 }
 
   has_many :book_authors
   has_many :authors, through: :book_authors
@@ -17,10 +18,10 @@ class Book < ApplicationRecord
   end
 
   def self.new_from_form(book_info)
-    if book_info['cover'] == ""
-      book_info['cover'] = "https://smartmobilestudio.com/wp-content/uploads/2012/06/leather-book-preview.png"
+    if book_info[:cover] == ""
+      book_info[:cover] = "https://smartmobilestudio.com/wp-content/uploads/2012/06/leather-book-preview.png"
     else
-      book_info['cover']
+      book_info[:cover]
     end
     self.create(book_info)
   end
