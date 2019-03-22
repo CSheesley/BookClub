f965cb9d0d03a76910f2d7325df1f2a60fc81e41
Corey Sheesley <corey.sheesley@gmail.com>
2019-03-21 20:04:07 -0600

    Adds test for adding or updating author

--- spec/features/books/new_spec.rb
+++ spec/features/books/new_spec.rb
@@ -32,5 +32,28 @@ RSpec.describe 'new book workflow', type: :feature do
       book = Book.last
       expect(current_path).to eq(book_path(book))
     end
+
+    it 'adds a new book to an existing author, otherwise it adds a new author' do
+      author_1 = Author.create(name: "Dan Brown")
+
+      visit new_book_path
+
+      book_title = "The Davinci Code"
+      book_pages = 350
+      book_year = 1995
+      book_cover = "madeupurl.com"
+      book_authors = "Dan Brown"
+
+      fill_in 'book[title]', with: book_title
+      fill_in 'book[pages]', with: book_pages
+      fill_in 'book[authors]', with: book_authors
+      fill_in 'book[year]', with: book_year
+      fill_in 'book[cover]', with: book_cover
+
+      click_button 'Create Book'
+
+      expect(Author.count).to eq(1)
+      expect(Author.first.books.count).to eq(1)
+    end
   end
 end
