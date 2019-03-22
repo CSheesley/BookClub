314c731debff1946376ce6a274031c4cf8800de8
Corey Sheesley <corey.sheesley@gmail.com>
2019-03-21 18:44:32 -0600

    Adds test to validate that new book pages is 1 or greater

--- spec/models/books/book_spec.rb
+++ spec/models/books/book_spec.rb
@@ -43,7 +43,7 @@ RSpec.describe Book, type: :model do
       expect(book_2.co_authors(author_2)).to eq(["Corey Sheesley"])
     end
 
-    it 'can prepulate a default cover image link if no image is provided' do
+    it 'can populate a default cover image link if no image is provided' do
       book_info_1 = ({title: "The Hobbit", pages: 200, year: 1999, cover: "madeupurl.com"})
       book_info_2 = ({title: "Blank Book", pages: 100, year: 1980, cover: ""})
 
@@ -54,6 +54,21 @@ RSpec.describe Book, type: :model do
 
       expect(book_1.cover).to eq("madeupurl.com")
       expect(book_2.cover).to eq(default_image)
+    end
+
+    it 'will not create a book if the page count is less than 1 page' do
+      book_info_1 = ({title: "The Hobbit", pages: 200, year: 1999, cover: "madeupurl.com"})
+      book_info_2 = ({title: "Bad Example", pages: 1, year: 1980, cover: "madeupurl.com"})
+      book_info_3 = ({title: "Worse Example", pages: 0, year: 1981, cover: "madeupurl.com"})
+      book_info_3 = ({title: "Worse Example", pages: -10, year: 1981, cover: "madeupurl.com"})
+      book_info_4 = ({title: "Worsest Example", pages: -10, year: 1981, cover: "madeupurl.com"})
+
+      book_1 = Book.new_from_form(book_info_1)
+      book_2 = Book.new_from_form(book_info_2)
+      book_3 = Book.new_from_form(book_info_3)
+      book_4 = Book.new_from_form(book_info_4)
+
+      expect(Book.count).to eq(2)
     end
   end
 end
