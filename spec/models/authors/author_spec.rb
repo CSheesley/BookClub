a8a5fe3eb5c7f8607d2555c11f690981cdce8993
Corey Sheesley <corey.sheesley@gmail.com>
2019-03-21 20:06:26 -0600

    Updates author_spec

--- spec/models/authors/author_spec.rb
+++ spec/models/authors/author_spec.rb
@@ -3,6 +3,13 @@ require 'rails_helper'
 RSpec.describe Author, type: :model do
   describe 'Validations' do
     it { should validate_presence_of :name}
+
+    it 'validates that name is uniqe' do
+      author_1 = Author.create(name: "Dan Brown")
+      author_2 = Author.create(name: "Dan Brown")
+
+      expect(Author.count).to eq(1)
+    end
   end
 
   describe 'Relationships' do
@@ -17,7 +24,7 @@ RSpec.describe Author, type: :model do
 
       author_list_1 = Author.authors_from_string(author_string_1)
       author_list_2 = Author.authors_from_string(author_string_2)
-      
+
       expect(author_list_1[0].name).to eq(author_string_1)
       expect(author_list_1.length).to eq(1)
 
