a4c3a90c799f9e4bff22a813316f3270073267d2
wipegup <wipegup@gmail.com>
2019-03-22 13:37:19 -0600

    Add test for average rating from group of reviews

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -55,5 +55,26 @@ RSpec.describe Review, type: :model do
       most_reviews = Review.most_reviews
       expect(most_reviews).to eq(["User_1", "User_2","User_3"])
     end
+    it '.review_ag returns avg rating for a collection of reviews' do
+
+      author_1 = Author.create(name: "J.R.R Tolkein")
+      author_2 = Author.create(name: "William Peterson")
+      author_3 = Author.create(name: "Corey Sheesley")
+
+      book_1 = author_1.books.create(title: "The Hobbit", pages: 200, year: 1999, cover: "madeupurl.com")
+      book_2 = author_2.books.create(title: "Title_2", pages: 400, year: 1998, cover: "othermadeupurl.com")
+      book_2.authors << author_3
+      book_3 = author_2.books.create(title: "Art of Data", pages: 400, year: 2000, cover: "otherothermadeupurl.com")
+
+      review_1 = book_1.reviews.create(title: "Great Book" , text: "What an adventure", rating: 5, user: "User_1")
+      review_2 = book_2.reviews.create(title: "Nice Read" , text: "Very enjoyable", rating: 4, user: "User_1")
+      review_3 = book_2.reviews.create(title: "If you have too" , text: "Meh", rating: 3, user: "User_2")
+      review_4 = book_3.reviews.create(title: "Data Pro" , text: "What a list!", rating: 5, user: "User_2")
+      review_5 = book_3.reviews.create(title: "Data Pro1" , text: "What a list!", rating: 5, user: "User_3")
+      review_6 = book_3.reviews.create(title: "Data Pro2" , text: "What a list!", rating: 5, user: "User_1")
+
+      review_avg = book_2.review.avg_rating
+      expect(review_avg).to eq(3.5)
+    end
   end
 end
