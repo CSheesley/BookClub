f37edd1b4c96cd04de5506b5cc36e539ab8a528a
wipegup <wipegup@gmail.com>
2019-03-23 10:42:46 -0600

    User Partial working

--- spec/features/user/partial_spec.rb
+++ spec/features/user/partial_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'user partial render', type: :view do
     book_1 = Book.create(title: "The Hobbit", pages: 200, year: 1999, cover: "madeupurl.com")
     review_1 = book_1.reviews.create(title: "Great Book" , text: "What an adventure", rating: 5, user: "User_1")
 
-    render user, user: review_1.user
+    render 'application/user', user: review_1.user
 
     expect(response).to have_xpath("//div[@id='user-#{review_1.user}']")
     within "#user-#{review_1.user}" do
