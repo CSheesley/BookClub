315ddd0770e6b5b48cff363729314767dbb6578f
Corey Sheesley <corey.sheesley@gmail.com>
2019-03-23 12:29:05 -0600

    Adds initial test for navbar funtionality

---
+++ spec/features/navbar_and_partials/navbar_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'navbar', type: :feature do
+  context 'as a visitor to the website' do
+    it 'should have a navbar with a link to all books index' do
+
+    visit '/'
+
+      within '#body' do
+        expect(page).to have_content("Welcome to BookClub")
+      end
+
+      within '#navbar' do
+        click_link 'All Books'
+        expect(current_path).to eq(books_path)
+      end
+    end
+
+    it 'should have a nav bar with a link back to the welcome page' do
+
+    visit 'books_path'
+
+      within '#navbar' do
+        click_link 'BookClub'
+        expect(current_path).to eq('/')
+      end
+    end
+  end
+end
