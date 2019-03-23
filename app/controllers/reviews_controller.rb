74f005c02d459c72ec91e17bbedacbfbc075fc50
Corey Sheesley <corey.sheesley@gmail.com>
2019-03-23 11:31:14 -0600

    Changes create method to new_from_form in reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,7 +11,7 @@ class ReviewsController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id])
-    @review = @book.reviews.create(review_params)
+    @review = @book.reviews.new_from_form(review_params)
     redirect_to book_path(@book)
   end
 
