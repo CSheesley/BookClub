b6f2a0e00fe2fd9f2658d85e565d7ffec9e2c631
Corey Sheesley <corey.sheesley@gmail.com>
2019-03-23 09:35:06 -0600

    Updates routes and links for new review form
    Co-authored-by: William Peterson <29714514+wipegup@users.noreply.github.com>

--- spec/features/reviews/new_spec.rb
+++ spec/features/reviews/new_spec.rb
@@ -19,16 +19,16 @@ RSpec.describe 'new review workflow', type: :feature do
     end
   end
 
-  # context 'as a user filling out a new review form' do
-  #   xit 'should accept input for a new review of a book, including title, user, rating, text' do
-  #     book_1 = Book.create(title: "The Hobbit", pages: 200, year: 1999, cover: "madeupurl.com")
-  #
-  #     # visit
-  #
-  #     review_title = "Best Ever"
-  #     review_user = "User_1"
-  #     review_rating = 5
-  #     review_text = "Here is a long description of the review for the book"
-  #   end
-  # end
+  context 'as a user filling out a new review form' do
+    it 'should accept input for a new review of a book, including title, user, rating, text' do
+      book_1 = Book.create(title: "The Hobbit", pages: 200, year: 1999, cover: "madeupurl.com")
+
+      visit
+
+      review_title = "Best Ever"
+      review_user = "User_1"
+      review_rating = 5
+      review_text = "Here is a long description of the review for the book"
+    end
+  end
 end
