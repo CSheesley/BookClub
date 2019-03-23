49b945e88277ae412bbdde910770e87b20c66a19
Corey Sheesley <corey.sheesley@gmail.com>
2019-03-23 10:08:49 -0600

    Adds test for creating review
    Co-authored-by: William Peterson <29714514+wipegup@users.noreply.github.com>

--- spec/features/reviews/new_spec.rb
+++ spec/features/reviews/new_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe 'new review workflow', type: :feature do
   end
 
   context 'as a user filling out a new review form' do
-    it 'should accept input for a new review of a book, including title, user, rating, text' do
+    it 'should accept input for a new review of a book, including title, user, rating, text - and redirect to book show page and has new review content' do
       book_1 = Book.create(title: "The Hobbit", pages: 200, year: 1999, cover: "madeupurl.com")
 
       visit new_book_review_path(book_1)
@@ -36,8 +36,12 @@ RSpec.describe 'new review workflow', type: :feature do
       fill_in 'review[text]', with: review_text
 
       click_button 'Create Review'
-      
+
+      review = Review.last
+
       expect(current_path).to eq(book_path(book_1))
+      expect(review.title).to eq(review_title)
+      expect(review.text).to eq(review_text)
     end
   end
 end
