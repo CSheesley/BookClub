fb4b4da39c747736c34e707e69edd08bad0674cf
Corey Sheesley <corey.sheesley@gmail.com>
2019-03-23 09:29:42 -0600

    Changes expected path to use nested path helper
    Co-authored-by: William Peterson <29714514+wipegup@users.noreply.github.com>

--- spec/features/reviews/new_spec.rb
+++ spec/features/reviews/new_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe 'new review workflow', type: :feature do
 
       click_link 'New Review'
 
-      expect(current_path).to eq(new_review_path)
+      expect(current_path).to eq(new_book_review_path(book_1))
 
       expect(page).to have_content("Title")
       expect(page).to have_content("User")
@@ -18,4 +18,17 @@ RSpec.describe 'new review workflow', type: :feature do
       expect(page).to have_content("Text")
     end
   end
+
+  # context 'as a user filling out a new review form' do
+  #   xit 'should accept input for a new review of a book, including title, user, rating, text' do
+  #     book_1 = Book.create(title: "The Hobbit", pages: 200, year: 1999, cover: "madeupurl.com")
+  #
+  #     # visit
+  #
+  #     review_title = "Best Ever"
+  #     review_user = "User_1"
+  #     review_rating = 5
+  #     review_text = "Here is a long description of the review for the book"
+  #   end
+  # end
 end
