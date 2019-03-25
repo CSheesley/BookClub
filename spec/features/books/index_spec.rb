6bd22df674a3cae7ab4897261a8f045fe8019516
Corey Sheesley <corey.sheesley@gmail.com>
2019-03-25 08:29:52 -0600

    Refactores book_index testing, accounting for testing in partials
    Co-authored-by: William Peterson <29714514+wipegup@users.noreply.github.com>

--- spec/features/books/index_spec.rb
+++ spec/features/books/index_spec.rb
@@ -3,72 +3,21 @@ require 'rails_helper'
 RSpec.describe "book index page", type: :feature do
   context 'as a visitor' do
     context 'when I visit the book index page' do
-      it 'shows all book titles in the database' do
+      it 'each book entry shows all book card' do
         author_1 = Author.create(name: "J.R.R Tolkein")
         author_2 = Author.create(name: "William Peterson")
-        author_3 = Author.create(name: "Corey Sheesley")
-        author_4 = Author.create(name: "Dan Brown")
 
         book_1 = author_1.books.create(title: "The Hobbit", pages: 310, year: 1937, cover: 'http://differentmadeupurl.com')
         book_2 = author_2.books.create(title: "Best Website Ever", pages: 100, year: 2018, cover: 'http://madeupurl.com')
-        book_2.authors << author_3
-        book_3 = author_4.books.create(title: "The Davinci Code", pages: 200, year: 2001, cover: 'http://anothermadeupurl.com')
 
         visit books_path
 
-        expect(page).to have_content(book_1.title)
-        expect(page).to have_content(book_2.title)
-        expect(page).to have_content(book_3.title)
-      end
-
-      it 'each book entry shows the author(s) who wrote the book, number of pages, and year published' do
-        author_1 = Author.create(name: "J.R.R Tolkein")
-        author_2 = Author.create(name: "William Peterson")
-        author_3 = Author.create(name: "Corey Sheesley")
-
-        book_1 = author_1.books.create(title: "The Hobbit", pages: 310, year: 1937, cover: 'http://differentmadeupurl.com')
-        book_2 = author_2.books.create(title: "Best Website Ever", pages: 100, year: 2018, cover: 'http://madeupurl.com')
-        book_2.authors << author_3
-
-        visit books_path
-
-        within "#book-card-#{book_1.id}" do
-          expect(page).to have_content(book_1.title)
-          # expect(page).to have_content("Author(s): #{book_1.authors.name}")
-          expect(page).to have_content("Pages: #{book_1.pages}")
-          expect(page).to have_content("Year of Publication: #{book_1.year}")
-        end
-
-        within "#book-card-#{book_2.id}" do
-          expect(page).to have_content(book_2.title)
-          # expect(page).to have_content("Author(s): #{book_2.authors.name}")
-          expect(page).to have_content("Pages: #{book_2.pages}")
-          expect(page).to have_content("Year of Publication: #{book_2.year}")
-        end
-      end
-
-      it 'each book entry shows a small thumbnail image of the book' do
-        author_1 = Author.create(name: "J.R.R Tolkein")
-        author_2 = Author.create(name: "William Peterson")
-        author_3 = Author.create(name: "Corey Sheesley")
-
-        book_1 = author_1.books.create(title: "The Hobbit", pages: 310, year: 1937, cover: 'http://differentmadeupurl.com')
-        book_2 = author_2.books.create(title: "Best Website Ever", pages: 100, year: 2018, cover: 'http://madeupurl.com')
-        book_2.authors << author_3
-
-        visit books_path
-
-        within "#book-card-#{book_1.id}" do
-          expect(page).to have_xpath("//img[@src='#{book_1.cover}']")
-        end
-        within "#book-card-#{book_2.id}" do
-          expect(page).to have_xpath("//img[@src='#{book_2.cover}']")
-        end
+        expect(page).to have_selector('div', id:"book-card-#{book_1.id}")
+        expect(page).to have_selector('div', id:"book-card-#{book_2.id}")
       end
     end
   end
 end
-
 
     # xit 'shows the average rating next to each book title' do
     #   author_1 = Author.create(name: "J.R.R Tolkein")
