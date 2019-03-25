c48bedfa32aaa2da5cdfb68e240e3ab4d10877cb
wipegup <wipegup@gmail.com>
2019-03-25 08:27:00 -0600

    Reformat tests for book partials; passing test
    Co-authored-by: Corey Sheesley <43529041+CSheesley@users.noreply.github.com>

--- spec/features/books/partial_spec.rb
+++ spec/features/books/partial_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'book partial render', type: :view do
-  it 'Should render the view of a book with one author' do
+  it 'Should render the all book information of a book with one author' do
     author_1 = Author.create(name: "J.R.R Tolkein")
 
     book_1 = author_1.books.create(title: "The Hobbit", pages: 200, year: 1999, cover: "madeupurl.com")
 
     render book_1
+
     expect(rendered).to have_selector('div', id:"book-card-#{book_1.id}")
     expect(rendered).to have_selector('div', id:"book-title", text:book_1.title)
     expect(rendered).to have_selector('div', id:"book-pages", text:book_1.pages)
@@ -14,26 +15,36 @@ RSpec.describe 'book partial render', type: :view do
     expect(rendered).to have_selector('div', id:"book-cover")
     expect(rendered).to have_xpath("//img[@src='#{book_1.cover}']")
     expect(rendered).to have_selector('div', id:"author-#{author_1.id}")
+  end
 
+  it 'Should render the all book information of a book with two authors' do
+    author_1 = Author.create(name: "J.R.R Tolkein")
+    author_2 = Author.create(name: "Corey Sheesley")
+
+    book_1 = author_1.books.create(title: "The Hobbit", pages: 200, year: 1999, cover: "madeupurl.com")
+    book_1.authors << author_2
+    
+    render book_1
+
+    expect(rendered).to have_selector('div', id:"book-card-#{book_1.id}")
+    expect(rendered).to have_selector('div', id:"book-title", text:book_1.title)
+    expect(rendered).to have_selector('div', id:"book-pages", text:book_1.pages)
+    expect(rendered).to have_selector('div', id:"book-year", text:book_1.year)
+    expect(rendered).to have_selector('div', id:"book-cover")
+    expect(rendered).to have_xpath("//img[@src='#{book_1.cover}']")
+    expect(rendered).to have_selector('div', id:"author-#{author_1.id}")
+    expect(rendered).to have_selector('div', id:"author-#{author_2.id}")
 
   end
 
-  it 'Should render the view of a book with two authors' do
-    author_2 = Author.create(name: "William Peterson")
-    author_3 = Author.create(name: "Corey Sheesley")
+  it 'Should have a link to new review' do
+    author_1 = Author.create(name: "J.R.R Tolkein")
 
-    book_2 = author_2.books.create(title: "Title_2", pages: 400, year: 1998, cover: "othermadeupurl.com")
-    book_2.authors << author_3
+    book_1 = author_1.books.create(title: "The Hobbit", pages: 200, year: 1999, cover: "madeupurl.com")
 
-    render book_2
+    render book_1
+    expect(rendered).to have_xpath("//a[@href='#{new_book_review_path(book_1)}']")
+    expect(rendered).to have_selector('a', text: "New Review")
 
-    expect(response).to have_xpath("//div[@id='book-card-#{book_2.id}']")
-    within "#book-card-#{book_2.id}" do
-      expect(response).to have_content(book_2.title)
-      expect(response).to have_content("Author(s): #{book_2.author_names.join(", ")}")
-      expect(response).to have_content("Pages: #{book_2.pages}")
-      expect(response).to have_content("Year of Publication: #{book_2.year}")
-      expect(response).to have_xpath("//img[@src='#{book_2.cover}']")
-    end
   end
 end
