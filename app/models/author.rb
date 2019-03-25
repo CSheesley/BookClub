61011533a848c90e77d281cf72cf8517b0911fdc
wipegup <wipegup@gmail.com>
2019-03-24 19:27:55 -0600

    Passing test for blank author

--- app/models/author.rb
+++ app/models/author.rb
@@ -7,8 +7,9 @@ class Author < ApplicationRecord
   def self.authors_from_string(author_string)
     string_list = author_string.split(",")
     string_list.map do |string|
-      
-      self.find_or_create_by(name: string.strip.titleize)
-    end
+      if string.strip.titleize != ''
+        self.find_or_create_by(name: string.strip.titleize)
+      end
+    end.compact
   end
 end
--- spec/models/authors/author_spec.rb
+++ spec/models/authors/author_spec.rb
@@ -37,9 +37,9 @@ RSpec.describe Author, type: :model do
       author_string = "Dan Brown, "
 
       author_list = Author.authors_from_string(author_string)
-
       expect(author_list[0].name).to eq("Dan Brown")
       expect(author_list.length).to eq(1)
+
 
     end
   end
