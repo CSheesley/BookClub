29412824cbf82caa7a78709f4da8497ee38872ee
wipegup <wipegup@gmail.com>
2019-03-25 12:35:24 -0600

    Add tests for including reviews in author show page

--- spec/features/authors/show_spec.rb
+++ spec/features/authors/show_spec.rb
@@ -32,5 +32,42 @@ RSpec.describe 'author show page', type: :feature do
       expect(page).not_to have_selector("div", id:"author-#{author_2.id}")
 
     end
+
+    it 'should show top review for each book - if it has one' do
+
+      author_1 = Author.create(name: "J.R.R Tolkein")
+      author_3 = Author.create(name: "Corey Sheesley")
+
+      book_1 = author_1.books.create(title: "The Hobbit", pages: 200, year: 1999, cover: "madeupurl.com")
+      book_2 = author_1.books.create(title: "Title_2", pages: 400, year: 1998, cover: "othermadeupurl.com")
+      book_2.authors << author_3
+      book_3 = author_1.books.create(title: "Art of Data", pages: 400, year: 2000, cover: "otherothermadeupurl.com")
+
+      review_1 = book_2.reviews.create(title: "Nice Read" , text: "Very enjoyable", rating: 4, user: "User_1")
+      review_2 = book_2.reviews.create(title: "If you have too" , text: "Meh", rating: 3, user: "User_2")
+      review_3 = book_3.reviews.create(title: "Data Pro" , text: "What a list!", rating: 5, user: "User_2")
+      visit author_path(author_1)
+
+      # save_and_open_page
+      expect(page).to have_selector('div', id:'book-pair-1')
+      within "#book-pair-1" do
+        expect(page).not_to have_selector('div', id: /review-/)
+        expect(page).to have_selector('div', id:"book-card-#{book_1.id}")
+      end
+
+      expect(page).to have_selector('div', id:'book-pair-2')
+      within "#book-pair-2" do
+        expect(page).to have_selector('div', id:"review-#{review_1.id}")
+        expect(page).to have_selector('div', id:"book-card-#{book_2.id}")
+      end
+
+
+      expect(page).to have_selector('div', id:'book-pair-3')
+      within "#book-pair-3" do
+        expect(page).to have_selector('div', id:"review-#{review_3.id}")
+        expect(page).to have_selector('div', id:"book-card-#{book_3.id}")
+      end
+
+    end
   end
 end
