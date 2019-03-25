11bdf745b5a2e56e47538475721fe58364799b37
wipegup <wipegup@gmail.com>
2019-03-25 08:50:32 -0600

    Add tests for author display
    Co-authored-by: Corey Sheesley <43529041+CSheesley@users.noreply.github.com>

--- spec/features/books/partial_spec.rb
+++ spec/features/books/partial_spec.rb
@@ -15,6 +15,8 @@ RSpec.describe 'book partial render', type: :view do
     expect(rendered).to have_selector('div', id:"book-cover")
     expect(rendered).to have_xpath("//img[@src='#{book_1.cover}']")
     expect(rendered).to have_selector('div', id:"author-#{author_1.id}")
+    expect(rendered).to have_selector('div', id:"author-info", text: "By:")
+    
   end
 
   it 'Should render the all book information of a book with two authors' do
@@ -23,7 +25,7 @@ RSpec.describe 'book partial render', type: :view do
 
     book_1 = author_1.books.create(title: "The Hobbit", pages: 200, year: 1999, cover: "madeupurl.com")
     book_1.authors << author_2
-    
+
     render book_1
 
     expect(rendered).to have_selector('div', id:"book-card-#{book_1.id}")
@@ -34,7 +36,7 @@ RSpec.describe 'book partial render', type: :view do
     expect(rendered).to have_xpath("//img[@src='#{book_1.cover}']")
     expect(rendered).to have_selector('div', id:"author-#{author_1.id}")
     expect(rendered).to have_selector('div', id:"author-#{author_2.id}")
-
+    expect(rendered).to have_selector('div', id:"author-info", text: "By:")
   end
 
   it 'Should have a link to new review' do
@@ -45,6 +47,21 @@ RSpec.describe 'book partial render', type: :view do
     render book_1
     expect(rendered).to have_xpath("//a[@href='#{new_book_review_path(book_1)}']")
     expect(rendered).to have_selector('a', text: "New Review")
+
+  end
+
+  it 'Should print NO co-author information if there is only one author' do
+    author_1 = Author.create(name: "J.R.R Tolkein")
+    book_1 = author_1.books.create(title: "The Hobbit", pages: 200, year: 1999, cover: "madeupurl.com")
+
+    render book_1, author: author_1
+
+    expect(rendered).not_to have_selector('div', id:'author-info', text: "With:")
+    expect(rendered).not_to have_selector('div', id:'author-info', text: "By:")
+
+  end
+
+  it 'Should print ONLY the co-author name when specified' do
 
   end
 end
